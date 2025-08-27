8650b41471357e4e3041771cf3d944e2b2920ba9
Dev Bachani <dev22603@gmail.com>
2025-08-27 16:24:33 +0530

    removed useless code

--- backend/controllers/user.controllers.mjs
+++ backend/controllers/user.controllers.mjs
@@ -2,17 +2,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { pool } from "../db/db.mjs";
-import { userSchema } from "../utils/validators/user.validator.mjs";
-import {
-	CHECK_USER_EXISTS,
-	GET_USER_BY_EMAIL_ID,
-	GET_USERS,
-	INSERT_USER,
-} from "../queries/user.queries.mjs";
-import {
-	GLOBAL_ERROR_MESSAGES,
-	USER_FEEDBACK_MESSAGES,
-} from "../utils/constants/app.messages.mjs";
 
 // better signup
 const signup = async (req, res) => {
