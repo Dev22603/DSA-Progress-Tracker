a7072db379bc7ec55b411233a49b34ec6fb6d8f1
Dev Bachani <dev22603@gmail.com>
2025-12-02 22:40:29 +0530

    schema change

---
+++ backend/prisma/migrations/20251202171007_added_new_features_v3/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `company_tags` on the `sheets` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "sheets" DROP COLUMN "company_tags";
